aec09252e3b29d6815e98bdb042710bcb7f59cfd
CasperWLX <hampuskallberg@gmail.com>
2023-09-28 10:38:19 +0200

    first commit

--- src/com/hampus/demo/Main.java
+++ src/com/hampus/demo/Main.java
@@ -1,9 +1,25 @@
 package com.hampus.demo;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        int[] numbers = {0, 100, 1000};
 
+
+        try
+        {
+            for (int i = 0; i < numbers.length; i++)
+            {
+                System.out.println(numbers[i] - numbers[i+1]);
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException aioobe)
+        {
+            System.out.println("Listan är slut");
+            aioobe.printStackTrace();
+        }
     }
 }
