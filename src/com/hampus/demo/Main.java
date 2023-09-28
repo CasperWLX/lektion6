efdce907289677b08873340fc7ca94a48630f3b1
CasperWLX <hampuskallberg@gmail.com>
2023-09-28 10:40:14 +0200

    clean slate

--- src/com/hampus/demo/Main.java
+++ src/com/hampus/demo/Main.java
@@ -6,20 +6,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int[] numbers = {0, 100, 1000};
 
-
-        try
-        {
-            for (int i = 0; i < numbers.length; i++)
-            {
-                System.out.println(numbers[i] - numbers[i+1]);
-            }
-        }
-        catch (ArrayIndexOutOfBoundsException aioobe)
-        {
-            System.out.println("Listan är slut");
-            aioobe.printStackTrace();
-        }
     }
 }
